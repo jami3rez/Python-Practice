76f23e878e2c276e9a19aeb1828dc59ab848ab39
Jamie Wharton <48032389+jami3rez@users.noreply.github.com>
2023-03-18 15:17:31 +0000

    Add files via upload

---
+++ When_am_I_100.py
@@ -0,0 +1,11 @@
+name = input('What is your name?:')
+
+print('Hi, ' + name + '!')
+
+age = int(input('And how old are you?'))
+
+thisyear = int(input('Wow, you look awful. Ok, what year is it, ' + name + '?'))
+
+year = str((thisyear - age)+100)
+
+print('Well, in ' + year + ' you will be 100 years old so it\'s nearly over!')
